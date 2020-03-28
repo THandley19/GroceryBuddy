f1b1ab672c9f41ddd74fd08879c1cb7a45298d57
landmammal <evolpirate@gmail.com>
2020-03-28 12:09:01 -0500

    commenting what we are not using right now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,23 @@
 // var db = require("../models");
 
 module.exports = function(app) {
+  console.log(app);
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.json(dbExamples);
-    // });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    // db.Example.create(req.body).then(function(dbExample) {
-    //   res.json(dbExample);
-    // });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    // db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-    //   res.json(dbExample);
-    // });
-  });
+  // app.get("/api/examples", function(req, res) {
+  //   // db.Example.findAll({}).then(function(dbExamples) {
+  //   //   res.json(dbExamples);
+  //   // });
+  // });
+  // // Create a new example
+  // app.post("/api/examples", function(req, res) {
+  //   // db.Example.create(req.body).then(function(dbExample) {
+  //   //   res.json(dbExample);
+  //   // });
+  // });
+  // // Delete an example by id
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   // db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //   //   res.json(dbExample);
+  //   // });
+  // });
 };
