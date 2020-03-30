9850f5014e0bed0a66f36efc097d1895719f6ce9
Robert Finkley <rfinkley82@gmail.com>
2020-03-29 20:20:27 -0400

    Updated file structure, updated index.handlebars, main.handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,16 +5,16 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
-  // // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-  // // Render 404 page for any unmatched routes
-  // app.get("*", function(req, res) {
-  //   res.render("404");
-  // });
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
 };
