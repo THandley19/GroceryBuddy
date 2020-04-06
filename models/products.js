6204faa61c0565d30ac7b97d1f8e4f78eea768e4
THandley19 <trevor.handley@gmail.com>
2020-04-06 08:11:48 -0400

    no more "vendor not defined"

--- models/products.js
+++ models/products.js
@@ -1,35 +1,43 @@
 module.exports = function(sequelize, DataTypes) {
-    var Products = sequelize.define("Products", {
-      product_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      product_description: {
-        type: DataTypes.STRING
-      },
-      product_category: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      product_subcategory: {
-        type: DataTypes.STRING
-      },
-      product_brand: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      product_vendor_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      product_price: {
-        type: DataTypes.DECIMAL(5,2),
-        allowNull: false
-      }
-    });
+  var Products = sequelize.define("Products", {
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    product_description: {
+      type: DataTypes.STRING
+    },
+    product_category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    product_subcategory: {
+      type: DataTypes.STRING
+    },
+    product_brand: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    VendorId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    product_price: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false
+    }
+  });
 
-    Products.associate = function(models) {
-      Products.belongsTo(models.Vendors);
-    };
-    return Products;
+  Products.associate = function(models) {
+    Products.belongsTo(
+      models.Vendors,
+      { as: "vendors" },
+      {
+        foreignKey: {
+          allowNull: false
+        }
+      }
+    );
   };
+  return Products;
+};
